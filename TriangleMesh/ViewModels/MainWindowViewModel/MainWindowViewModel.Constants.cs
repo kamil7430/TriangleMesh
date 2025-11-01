44e85684d81bf69d76f255d4f9e61ae7ee975563
Kamil Błażejczyk <01189072@pw.edu.pl>
2025-11-01 23:42:15 +0100

    cleanup constants

--- TriangleMesh/ViewModels/MainWindowViewModel/MainWindowViewModel.Constants.cs
+++ TriangleMesh/ViewModels/MainWindowViewModel/MainWindowViewModel.Constants.cs
@@ -3,10 +3,10 @@ namespace TriangleMesh.ViewModels;
 public partial class MainWindowViewModel
 {
     // Read-only (constant) fields
-    public int MinTriangulationPrecision { get; } = 3;
-    public int MaxTriangulationPrecision { get; } = 30;
-    public double MinAlphaAngle { get; } = -90;
-    public double MaxAlphaAngle { get; } = 90;
-    public double MinBetaAngle { get; } = -90;
-    public double MaxBetaAngle { get; } = 90;
+    public static int MinTriangulationPrecision => 3;
+    public static int MaxTriangulationPrecision => 30;
+    public static double MinAlphaAngle => -90;
+    public static double MaxAlphaAngle => 90;
+    public static double MinBetaAngle => -90;
+    public static double MaxBetaAngle => 90;
 }
