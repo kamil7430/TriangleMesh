0d17986f2219909355d9142c28c61c323072b32e
Kamil Błażejczyk <01189072@pw.edu.pl>
2025-11-16 20:12:15 +0100

    change angles to -180:180

--- TriangleMesh/ViewModels/MainWindowViewModel/MainWindowViewModel.Constants.cs
+++ TriangleMesh/ViewModels/MainWindowViewModel/MainWindowViewModel.Constants.cs
@@ -7,10 +7,10 @@ public partial class MainWindowViewModel
     // Read-only (constant) fields
     public static int MinTriangulationPrecision => 3;
     public static int MaxTriangulationPrecision => 30;
-    public static double MinAlphaAngle => -90;
-    public static double MaxAlphaAngle => 90;
-    public static double MinBetaAngle => -90;
-    public static double MaxBetaAngle => 90;
+    public static double MinAlphaAngle => -180;
+    public static double MaxAlphaAngle => 180;
+    public static double MinBetaAngle => -180;
+    public static double MaxBetaAngle => 180;
     public static double MinComponentValue => 0;
     public static double MaxComponentValue => 1;
     public static double LightRotationAngleSpeedInRadiansPerTick => 2.0.ToRadians();
